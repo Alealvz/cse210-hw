2f2939eafe460b3d0a928ea8b1521920b0f599bb
alealvz <alejandro.alvarez.cabezas@gmail.com>
2024-09-28 09:23:21 -0600

    add all changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Field Technology Specialist";
+        job1._company = "FedEx Office";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Assistant Manager";
+        job2._company = "FedEx Office";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Alejandro Alvarez";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
